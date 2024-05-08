6c08809de2923827c4ba3f702e3f9a6401b7d330
Milad Tahanian <miladtahanianofficial@gmail.com>
2024-05-08 15:56:39 +0330

    Update config.py

--- config.py
+++ config.py
@@ -65,7 +65,7 @@ updated_hour = current_date_time.strftime("%H")
 updated_minute = current_date_time.strftime("%M")
 
 # Combine the strings to form the final result
-final_string = f"{current_day} - {current_month} | {updated_hour}:{updated_minute}"
+final_string = f"{current_month}--{current_day} | {updated_hour}:{updated_minute}"
 config_string = "#✅ " + str(final_string) + "-"
 
 for code in codes:
